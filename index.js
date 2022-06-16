33a2056ff0d1eb120448ff4fc2e04abd872e1d5d
JOY <joy.kemunto@student.moringaschool.com>
2022-06-16 16:13:31 +0300

    commit tree 4720257504469b355e0b6230d571618d2d4eb9c7

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i;
+    // if any of the letters don't match, return false
+    if (word[i] !== word[j]) return false;
+  }
+
+  // return true
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
-*/
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
